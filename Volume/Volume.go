950ef31d6dce2fda58ab1218427bde2c213a94a7
illua1 <fubdmgF@github.com>
2022-04-23 11:58:39 +0300

    Box containers

--- Volume/Volume.go
+++ Volume/Volume.go
@@ -57,61 +57,61 @@ func (p Point[Value]) Eq(q Point[Value]) bool {
 	return p == q
 }
 
-func (p Point[Value]) And(s Point[Value]) Boxe[Value] {
-	return Boxe[Value]{
+func (p Point[Value]) And(s Point[Value]) Box[Value] {
+	return Box[Value]{
 		Min: Point[Value]{p.X, p.Y, p.Z},
 		Max: Point[Value]{p.X + s.X, p.Y + s.Y, p.Z + s.Z},
 	}
 }
 
-func (p Point[Value]) In(b Boxe[Value]) bool {
+func (p Point[Value]) In(b Box[Value]) bool {
 	return ((b.Min.X <= p.X) && (b.Min.Y <= p.Y) && (b.Min.Z <= p.Z)) && ((b.Max.X >= p.X) && (b.Max.Y >= p.Y) && (b.Max.Z >= p.Z))
 }
 
-type Boxe[Value value.Values] struct {
+type Box[Value value.Values] struct {
 	Min, Max Point[Value]
 }
 
-func NewBoxe[Value value.Values](min_x, min_y, min_z, max_x, max_y, max_z Value) Boxe[Value] {
-	return Boxe[Value]{
+func NewBox[Value value.Values](min_x, min_y, min_z, max_x, max_y, max_z Value) Box[Value] {
+	return Box[Value]{
 		Min: Point[Value]{min_x, min_y, min_z},
 		Max: Point[Value]{max_x, max_y, max_z},
 	}
 }
 
-func (b Boxe[Value]) String() string {
+func (b Box[Value]) String() string {
 	return fmt.Sprint(b.Min) + "-" + fmt.Sprint(b.Max)
 }
 
-func (b Boxe[Value]) Dx() Value {
+func (b Box[Value]) Dx() Value {
 	return b.Max.X - b.Min.X
 }
 
-func (b Boxe[Value]) Dy() Value {
+func (b Box[Value]) Dy() Value {
 	return b.Max.Y - b.Min.Y
 }
 
-func (b Boxe[Value]) Dz() Value {
+func (b Box[Value]) Dz() Value {
 	return b.Max.Z - b.Min.Z
 }
 
-func (b Boxe[Value]) Size() Point[Value] {
+func (b Box[Value]) Size() Point[Value] {
 	return b.Max.Sub(b.Min)
 }
 
-func (b Boxe[Value]) Add(p Point[Value]) Boxe[Value] {
-	return Boxe[Value]{b.Min.Add(p), b.Max.Add(p)}
+func (b Box[Value]) Add(p Point[Value]) Box[Value] {
+	return Box[Value]{b.Min.Add(p), b.Max.Add(p)}
 }
 
-func (b Boxe[Value]) Sub(p Point[Value]) Boxe[Value] {
-	return Boxe[Value]{b.Min.Sub(p), b.Max.Sub(p)}
+func (b Box[Value]) Sub(p Point[Value]) Box[Value] {
+	return Box[Value]{b.Min.Sub(p), b.Max.Sub(p)}
 }
 
-func (b Boxe[Value]) Mul(p Value) Boxe[Value] {
-	return Boxe[Value]{b.Min.Mul(p), b.Max.Mul(p)}
+func (b Box[Value]) Mul(p Value) Box[Value] {
+	return Box[Value]{b.Min.Mul(p), b.Max.Mul(p)}
 }
 
-func (b Boxe[Value]) Intersect(v Boxe[Value]) Boxe[Value] {
+func (b Box[Value]) Intersect(v Box[Value]) Box[Value] {
 	if b.Min.X < v.Min.X {
 		b.Min.X = v.Min.X
 	}
@@ -134,7 +134,7 @@ func (b Boxe[Value]) Intersect(v Boxe[Value]) Boxe[Value] {
 	return b
 }
 
-func (b Boxe[Value]) Union(v Boxe[Value]) Boxe[Value] {
+func (b Box[Value]) Union(v Box[Value]) Box[Value] {
 	if b.Min.X > v.Min.X {
 		b.Min.X = v.Min.X
 	}
@@ -157,7 +157,7 @@ func (b Boxe[Value]) Union(v Boxe[Value]) Boxe[Value] {
 	return b
 }
 
-func (b Boxe[Value]) Canon() Boxe[Value] {
+func (b Box[Value]) Canon() Box[Value] {
 	if b.Min.X < b.Max.X {
 		b.Min.X, b.Max.X = b.Max.X, b.Min.X
 	}
@@ -170,11 +170,11 @@ func (b Boxe[Value]) Canon() Boxe[Value] {
 	return b
 }
 
-func (b Boxe[Value]) Centre() Point[Value] {
+func (b Box[Value]) Centre() Point[Value] {
 	return b.Min.Add(b.Max).Div(2)
 }
 
-func (b Boxe[Value]) Points() [8]Point[Value] {
+func (b Box[Value]) Points() [8]Point[Value] {
 	return [8]Point[Value]{
 		b.Min,
 		Point[Value]{b.Min.X, b.Min.Y, b.Max.Z},
@@ -187,7 +187,7 @@ func (b Boxe[Value]) Points() [8]Point[Value] {
 	}
 }
 
-func (b Boxe[Value]) FaceCentres() [6]Point[Value] {
+func (b Box[Value]) FaceCentres() [6]Point[Value] {
 	centre := b.Centre()
 	return [6]Point[Value]{
 		Point[Value]{centre.X, centre.Y, b.Min.Z},
@@ -199,7 +199,7 @@ func (b Boxe[Value]) FaceCentres() [6]Point[Value] {
 	}
 }
 
-func (b Boxe[Value]) Edges() [12][2]int {
+func (b Box[Value]) Edges() [12][2]int {
 	return [12][2]int{
 		[2]int{0, 1},
 		[2]int{1, 2},
@@ -218,7 +218,7 @@ func (b Boxe[Value]) Edges() [12][2]int {
 	}
 }
 
-func (b Boxe[Value]) FaceEdges() [6][4]int {
+func (b Box[Value]) FaceEdges() [6][4]int {
 	return [6][4]int{
 		[4]int{0, 4, 8, 9},
 		[4]int{1, 5, 9, 10},
@@ -229,7 +229,7 @@ func (b Boxe[Value]) FaceEdges() [6][4]int {
 	}
 }
 
-func (b Boxe[Value]) FacePoints() [6][4]int {
+func (b Box[Value]) FacePoints() [6][4]int {
 	return [6][4]int{
 		[4]int{0, 1, 2, 3},
 		[4]int{4, 5, 6, 7},
@@ -240,7 +240,7 @@ func (b Boxe[Value]) FacePoints() [6][4]int {
 	}
 }
 
-func (b Boxe[Value]) FaceArea() [6][2]Value {
+func (b Box[Value]) FaceArea() [6][2]Value {
 	dx, dy, dz := b.Dx(), b.Dy(), b.Dz()
   dx, dy, dz = dx/2, dy/2, dz/2
   return [6][2]Value{
@@ -253,16 +253,134 @@ func (b Boxe[Value]) FaceArea() [6][2]Value {
 	}
 }
 
+const (
+  TopFace = iota
+  BottomFace
+  LeftFace
+  RigthFace
+  FrontFace
+  TailFace
+)
+
+const (
+  A_top_Edges = iota
+  B_top_Edges
+  C_top_Edges
+  D_top_Edges
+  A_bottom_Edges
+  B_bottom_Edges
+  C_bottom_Edges
+  D_bottom_Edges
+  A_centre_Edges
+  B_centre_Edges
+  C_centre_Edges
+  D_centre_Edges
+)
+
+const (
+  A_Point = iota
+  B_Point
+  C_Point
+  D_Point
+  E_Point
+  F_Point
+  G_Point
+  H_Point
+)
+
 /*
-func (b Boxe[Value]) FaceCentres() (ret [6]Point[Value]){
-	points := b.Points()
-  f_indeces := b.FacePoints()
-  for i := range f_indeces {
-    for p := range f_indeces[i] {
-      ret[i] = ret[i].Add(points[f_indeces[i][p]])
+  
+  TopFace = {
+    A_top_Edges = {
+      A_Point
+      B_Point
     }
-    ret[i] = ret[i].Div(4)
+    B_top_Edges = {
+      B_Point
+      C_Point
+    }
+    C_top_Edges = {
+      C_Point
+      D_Point
+    }
+    D_top_Edges = {
+      D_Point
+      A_Point
+    }
   }
-  return
-}
+  
+  BottomFace = {
+    A_bottom_Edges = {
+      E_Point
+      F_Point
+    }
+    B_bottom_Edges = {
+      F_Point
+      G_Point
+    }
+    C_bottom_Edges = {
+      G_Point
+      H_Point
+    }
+    D_bottom_Edges = {
+      H_Point
+      E_Point
+    }
+  }
+  
+  LeftFace = {
+    A_top_Edges
+    A_centre_Edges
+    A_bottom_Edges
+    B_centre_Edges
+  }
+  
+  FrontFace = {
+    B_top_Edges
+    B_centre_Edges
+    B_bottom_Edges
+    C_centre_Edges
+  }
+  
+  RigthFace = {
+    C_top_Edges
+    C_centre_Edges
+    C_bottom_Edges
+    D_centre_Edges
+  }
+  
+  TailFace = {
+    D_top_Edges
+    D_centre_Edges
+    D_bottom_Edges
+    A_centre_Edges
+  }
+  
+  A_centre_Edges = {
+    A_Point
+    E_Point
+  }
+  
+  B_centre_Edges = {
+    B_Point
+    F_Point
+  }
+  
+  C_centre_Edges = {
+    C_Point
+    G_Point
+  }
+  
+  D_centre_Edges = {
+    D_Point
+    H_Point
+  }
+  
+  
 */
+
+type BoxContainerFaces[T any] [6]T
+
+type BoxContainerEdges[T any] [12]T
+
+type BoxContainerPoints[T any] [8]T
